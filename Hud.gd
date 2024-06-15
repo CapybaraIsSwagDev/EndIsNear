f3cd83f09eeaa2b0c2be6ceba13b09f0f335d3f5
juraj863 <majtan.juraj4@gmail.com>
2024-06-15 11:53:39 +0200

    debug and friction

--- Hud.gd
+++ Hud.gd
@@ -3,6 +3,7 @@ class_name ui
 
 @onready var oxygen_label = $VBoxContainer/OxygenLabel
 @onready var energy_label = $VBoxContainer/EnergyLabel
+@onready var debug = $VBoxContainer/debug
 
 
 
@@ -16,10 +17,11 @@ func _process(delta):
 	pass
 
 # Updates Labels like Oxygen and Energy
-func _update_labels(Oxygen,Energy):
+func _update_labels(Oxygen,Energy,speed):
 	oxygen_label.text = "Oxygen: " + str(Oxygen)
 	energy_label.text = "Energy: " + str(Energy)
+	debug.text = str(speed)
 
 
-func _on_body_update_stats(Oxygen, Energy):
-	_update_labels(Oxygen,Energy)
+func _on_body_update_stats(Oxygen, Energy,speed):
+	_update_labels(Oxygen,Energy,speed)
--- player.gd
+++ player.gd
@@ -1,22 +1,37 @@
 extends CharacterBody2D
 class_name player
 
-signal UpdateStats(Oxygen,Energy)
+signal UpdateStats(Oxygen,Energy,speed)
 
 var Oxygen = 100
 var Energy = 69
 
 
-var speed = 400  # move speed in pixels/sec
+var speed = 10  # move speed in pixels/sec
+var Acclereration = 1
+var friction = 0.1
 var rotation_speed = 1.5  # turning speed in radians/sec
-
+var vel = Vector2(0,0)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	emit_signal("UpdateStats",Oxygen,Energy)
+	emit_signal("UpdateStats",Oxygen,Energy,vel)
 
 func _physics_process(delta):
 	var move_input = Input.get_axis("down", "up")
 	var rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.x * move_input * speed
+	vel = vel + (transform.x * move_input * Acclereration)
+		# Calculate friction force
+	var friction_force = vel * -friction
+	# Update velocity
+	var new_vel = vel + friction_force * delta
+	
+	# Ensure velocity doesn't reverse direction due to friction
+	if vel.length() > 0 and new_vel.length() > vel.length():
+		new_vel = Vector2(0, 0)
+	
+	
 	rotation += rotation_direction * rotation_speed * delta
+	velocity = new_vel
+	vel = new_vel
+	new_vel = Vector2(0,0)
 	move_and_slide()
