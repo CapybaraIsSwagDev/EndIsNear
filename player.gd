41d5f53aa7e34b59dbd2dfa6e602477d44bb60da
juraj863 <majtan.juraj4@gmail.com>
2024-06-15 12:16:13 +0200

    Update player.gd

--- player.gd
+++ player.gd
@@ -7,31 +7,32 @@ var Oxygen = 100
 var Energy = 69
 
 
-var speed = 10  # move speed in pixels/sec
-var Acclereration = 1
-var friction = 0.1
+var MaxSpeed = 80 # move speed in pixels/sec
+var Acclereration = 10
+var friction = 1
 var rotation_speed = 1.5  # turning speed in radians/sec
 var vel = Vector2(0,0)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	emit_signal("UpdateStats",Oxygen,Energy,vel)
+	emit_signal("UpdateStats",Oxygen,Energy,velocity)
 
 func _physics_process(delta):
 	var move_input = Input.get_axis("down", "up")
 	var rotation_direction = Input.get_axis("left", "right")
-	vel = vel + (transform.x * move_input * Acclereration)
+	velocity = velocity + (transform.x * move_input * Acclereration)
+	var current_speed = velocity.length()
 		# Calculate friction force
-	var friction_force = vel * -friction
+	var friction_force = velocity * -friction
 	# Update velocity
-	var new_vel = vel + friction_force * delta
+	var new_vel = velocity + friction_force * delta
 	
-	# Ensure velocity doesn't reverse direction due to friction
-	if vel.length() > 0 and new_vel.length() > vel.length():
+	# Ensure velocity doesn't reverse direction due to friction # MAGIC CODE FROM CHATGPT
+	if current_speed > 0 and new_vel.length() > current_speed:
 		new_vel = Vector2(0, 0)
+	#End of magic code
+	if current_speed > MaxSpeed:
+		velocity = velocity.normalized() * MaxSpeed
 	
-	
-	rotation += rotation_direction * rotation_speed * delta
+	rotation += rotation_direction * rotation_speed * delta # rotates ship
 	velocity = new_vel
-	vel = new_vel
-	new_vel = Vector2(0,0)
 	move_and_slide()
